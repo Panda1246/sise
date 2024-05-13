901d8f0456b9a208aab736b93890b5cd770fd686
despite-radio <242336@edu.p.lodz.pl>
2024-05-13 10:23:46 +0200

    Improve extractData

--- research/plots.py
+++ research/plots.py
@@ -11,15 +11,22 @@ def read_data(filename): # Function to read data from CSV file
         for row in reader: data.append(row)
     return data
 
-def extract_data(data, column_index): # Function to extract relevant data for plotting
+def extract_data(data, column_index, alg=None): # Function to extract relevant data for plotting
     extracted_data = {}
     for row in data:
-        difficulty = int(row[0])
+        difficulty = int(row[0]);
         algorithm = row[2]
-        if difficulty not in extracted_data: extracted_data[difficulty] = {}
-        if algorithm not in extracted_data[difficulty]: extracted_data[difficulty][algorithm] = []
 
-        extracted_data[difficulty][algorithm].append(float(row[column_index]))
+        if difficulty not in extracted_data: extracted_data[difficulty] = {}
+
+        if alg is None: # do not filter
+            if algorithm not in extracted_data[difficulty]: extracted_data[difficulty][algorithm] = []
+            extracted_data[difficulty][algorithm].append(float(row[column_index]))
+        elif algorithm == alg: # filter
+            detail = row[3]
+            if detail not in extracted_data[difficulty]: extracted_data[difficulty][detail] = []
+            extracted_data[difficulty][detail].append(float(row[column_index]))
+
     return extracted_data
 
 # Function to create bar plots
@@ -46,34 +53,47 @@ def plotType1(data, title, ylabel, filename):
 
 # Main function
 
-def globalPlots():
-    data = read_data('data.csv')
-
+def globalPlots(data):
     # Extract relevant data for plotting
-    solution_length_data = extract_data(data, 5)
-    visited_states_data = extract_data(data, 6)
-    processed_states_data = extract_data(data, 7)
-    time_data = extract_data(data, 8)
+    sl_data = extract_data(data, 5)
+    vs_data = extract_data(data, 6)
+    ps_data = extract_data(data, 7)
+    t_data = extract_data(data, 8)
 
     # typ 1
-    plotType1(time_data,
+    plotType1(t_data,
                     "Średni czas znalezienia rozwiązania w zależności od głębokości",
                     "Średni czas [ms]",
                     'glob_time_vs_diff.png')
-    plotType1(solution_length_data,
+    plotType1(sl_data,
                     "Średnia długość rozwiązania w zależności od głębokości",
                     "Średnia długość [ruchy]",
                     'glob_sol_len_vs_diff.png')
-    plotType1(visited_states_data,
+    plotType1(vs_data,
                     "Średnia liczba odwiedzonych stanów w zależności od głębokości",
                     "Średni numer odwiedzonych stanów",
                     'glob_vstd_c_vs_diff.png')
-    plotType1(processed_states_data,
+    plotType1(ps_data,
                     "Średnia liczba przetworzonych stanów w zależności od głębokości",
                     "Średni numer przetworzonych stanów",
                     'glob_proc_c_vs_diff.png')
+def astarPlots(data):
+    sl_data = extract_data(data, 5, alg="astr")
+    vs_data = extract_data(data, 6, alg="astr")
+    ps_data = extract_data(data, 7, alg="astr")
+    t_data =  extract_data(data, 8, alg="astr")
 
-def main():
-    globalPlots()
+    pass # TODO: implement this
+
+def dbfsPlots(data):
+    pass # TODO: implement this
     
+def main():
+    data = read_data('data.csv')
+
+    globalPlots(data)
+    astarPlots(data)
+    dbfsPlots(data)
+
+
 if __name__ == "__main__": main()
