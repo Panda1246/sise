c3d726274174d1027fbf3184480c1a65a1f130b6
despite-radio <242336@edu.p.lodz.pl>
2024-05-13 08:55:48 +0200

    Modernized script

--- logic/extdata.sh
+++ logic/extdata.sh
@@ -10,15 +10,17 @@
 #  size_depth_id_strategy_param_stats.txt
 # for example:
 #  4x4_01_00001_bfs_rdul_stats.txt
+declare -r \
+	dataFile="data.csv " \
+	stats_filename_regex='^[a-zA-Z0-9]+_([0-9]+)_([0-9]+)_([a-zA-Z]+)_([a-zA-Z]+)_stats.txt$'
 
-stats_filename_regex=\
-'^[a-zA-Z0-9]+_([0-9]+)_([0-9]+)_([a-zA-Z]+)_([a-zA-Z]+)_stats.txt$'
+[ -f "$dataFile" ] && rm "$dataFile" && echo "Removed old data file"
 
 for filename in *; do
     if [[ -f "$filename" && "$filename" =~ $stats_filename_regex ]]; then
         line=$(printf "%d %d %s %s " $((10#${BASH_REMATCH[1]})) \
                $((10#${BASH_REMATCH[2]})) ${BASH_REMATCH[3]} ${BASH_REMATCH[4]})
         line+=$(echo $(cat "$filename"))
-        echo "$line"
+        echo "$line" | sed 's/ /,/g' >> "$dataFile" # NOTE:musi być SED
     fi
 done
